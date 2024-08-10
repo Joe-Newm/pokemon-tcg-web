5c849ee62a24fa2ae159db87ba22bb42e87d27ba
joe-newm <j.newm500@gmail.com>
2024-08-10 09:23:04 -0500

    add .gitignore for .env variables

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import axios from "axios";
+import dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
+
+dotenv.config();
+const apiKey = process.env.API_KEY;
 
 app.use(express.urlencoded({ extended: true }));
 
