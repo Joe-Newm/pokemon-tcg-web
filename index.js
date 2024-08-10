1a01790b8b9bf2baea3a43525f584b33248edeb3
joe-newm <j.newm500@gmail.com>
2024-08-10 15:56:49 -0500

    add image search, shows all images/cards for pokemon

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
-const API_URL = "https://api.pokemontcg.io";
+const API_URL = "https://api.pokemontcg.io/v2/cards";
 
 dotenv.config();
 const apiKey = process.env.API_KEY;
@@ -12,8 +12,28 @@ const apiKey = process.env.API_KEY;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/", async (req, res) => {
+  res.render("index.ejs", { images: [] });
+});
+
+app.post("/submit", async (req, res) => {
+  const pokemonName = req.body.pokemonName;
+  let images = [];
+  try {
+    if (pokemonName) {
+      const result = await axios.get(`${API_URL}?q=name:${pokemonName}`, {
+        headers: {
+          "X-Api-Key": apiKey,
+        },
+      });
+      const pokedata = result.data.data;
+      images = pokedata.map((card) => card.images.small);
+    }
+    res.render("index.ejs", { images });
+  } catch (error) {
+    console.error("Error fetching data from API:", error.message);
+    res.status(500).send("Something went wrong!");
+  }
 });
 
 app.listen(port, () => {
