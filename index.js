5bb7e1872308413d30513605f44dcb2715d5bf0a
joe-newm <j.newm500@gmail.com>
2024-08-10 09:52:21 -0500

    add main.css with styles, partials files, boilerplate for html css

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ import dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
+const API_URL = "https://api.pokemontcg.io";
 
 dotenv.config();
 const apiKey = process.env.API_KEY;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
